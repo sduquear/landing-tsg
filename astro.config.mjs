941b1fdb35cd217163a6d116e6b4d591dc7a2063
Sebastián Duque <sebastian@owqlo.com>
2025-06-19 01:43:50 +0200

    feat: add site URL configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://totalsportsgroup.es",
   vite: {
     plugins: [tailwindcss()]
   },
